c466cbbcf3e0c1abf3986d635e28a3e5415e65bc
dogking-DK <56546271+dogking-DK@users.noreply.github.com>
2025-12-05 09:18:09 +0800

    Refactor resource loader registry

--- src/runtime/resource/cpu/ResourceLoader.cpp
+++ src/runtime/resource/cpu/ResourceLoader.cpp
@@ -1,4 +1,4 @@
-﻿#include "ResourceLoader.h"
+#include "ResourceLoader.h"
 
 #include <fmt/base.h>
 
@@ -7,7 +7,6 @@
 
 namespace dk {
 using namespace helper;
-
 namespace {
 std::shared_ptr<MeshData> decode_mesh_from_raw(const std::filesystem::path& path)
 {
@@ -33,13 +32,16 @@ RawMaterial decode_material_from_raw(const std::filesystem::path& path)
 {
     return read_pod<RawMaterial>(path);
 }
-} // namespace
 
-
-/* ---- Mesh ---- */
-std::shared_ptr<MeshData> ResourceLoader::loadMesh(UUID id)
+class MeshResourceLoader : public IResourceLoader<MeshData>
 {
-    return _cache.resolve<MeshData>(id, [&]
+public:
+    MeshResourceLoader(std::filesystem::path dir, AssetDB& db, ResourceLoader& owner)
+        : _dir(std::move(dir)), _db(db), _owner(owner)
+    {
+    }
+
+    std::shared_ptr<MeshData> load(UUID id) override
     {
         auto meta = _db.get(id);
         if (!meta)
@@ -49,20 +51,25 @@ std::shared_ptr<MeshData> ResourceLoader::loadMesh(UUID id)
         }
         auto m = decode_mesh_from_raw(_dir / meta->raw_path);
 
-        /* 材质依赖（可选）：meta.dependencies[material] = material uuid */
-        if (auto it = meta->dependencies.find(AssetDependencyType::Material);
-            it != meta->dependencies.end())
+        if (auto it = meta->dependencies.find(AssetDependencyType::Material); it != meta->dependencies.end())
         {
-            m->material = loadMaterial(it->second);
+            m->material = _owner.load<MaterialData>(it->second);
         }
         return m;
-    });
-}
+    }
 
-/* ---- Image ---- */
-std::shared_ptr<TextureData> ResourceLoader::loadImage(UUID id)
+private:
+    std::filesystem::path _dir;
+    AssetDB&              _db;
+    ResourceLoader&       _owner;
+};
+
+class TextureResourceLoader : public IResourceLoader<TextureData>
 {
-    return _cache.resolve<TextureData>(id, [&]
+public:
+    TextureResourceLoader(std::filesystem::path dir, AssetDB& db) : _dir(std::move(dir)), _db(db) {}
+
+    std::shared_ptr<TextureData> load(UUID id) override
     {
         auto meta = _db.get(id);
         if (!meta)
@@ -71,13 +78,22 @@ std::shared_ptr<TextureData> ResourceLoader::loadImage(UUID id)
             return std::shared_ptr<TextureData>{};
         }
         return decode_image_from_raw(_dir / meta->raw_path);
-    });
-}
+    }
 
-/* ---- Material ---- */
-std::shared_ptr<MaterialData> ResourceLoader::loadMaterial(UUID id)
+private:
+    std::filesystem::path _dir;
+    AssetDB&              _db;
+};
+
+class MaterialResourceLoader : public IResourceLoader<MaterialData>
 {
-    return _cache.resolve<MaterialData>(id, [&]
+public:
+    MaterialResourceLoader(std::filesystem::path dir, AssetDB& db, ResourceLoader& owner)
+        : _dir(std::move(dir)), _db(db), _owner(owner)
+    {
+    }
+
+    std::shared_ptr<MaterialData> load(UUID id) override
     {
         auto meta = _db.get(id);
         if (!meta)
@@ -89,17 +105,33 @@ std::shared_ptr<MaterialData> ResourceLoader::loadMaterial(UUID id)
         auto mat       = std::make_shared<MaterialData>();
         mat->metallic  = raw.metallic_factor;
         mat->roughness = raw.roughness_factor;
+        mat->base_color = raw.base_color;
         if (!raw.base_color_texture.is_nil())
-            mat->base_color_tex = loadImage(raw.base_color_texture);
+            mat->base_color_tex = _owner.load<TextureData>(raw.base_color_texture);
         if (!raw.metal_rough_texture.is_nil())
-            mat->metal_rough_tex = loadImage(raw.metal_rough_texture);
+            mat->metal_rough_tex = _owner.load<TextureData>(raw.metal_rough_texture);
         if (!raw.occlusion_texture.is_nil())
-            mat->occlusion_tex = loadImage(raw.occlusion_texture);
+            mat->occlusion_tex = _owner.load<TextureData>(raw.occlusion_texture);
         if (!raw.normal_texture.is_nil())
-            mat->normal_tex = loadImage(raw.normal_texture);
+            mat->normal_tex = _owner.load<TextureData>(raw.normal_texture);
         if (!raw.emissive_texture.is_nil())
-            mat->emissive_tex = loadImage(raw.emissive_texture);
+            mat->emissive_tex = _owner.load<TextureData>(raw.emissive_texture);
         return mat;
-    });
+    }
+
+private:
+    std::filesystem::path _dir;
+    AssetDB&              _db;
+    ResourceLoader&       _owner;
+};
+} // namespace
+
+ResourceLoader::ResourceLoader(const std::string& rawDir, AssetDB& db, ResourceCache& cache)
+    : _dir(rawDir), _db(db), _cache(cache)
+{
+    registerLoader<MeshData, MeshResourceLoader>(AssetType::Mesh, "Mesh", _dir, _db, *this);
+    registerLoader<TextureData, TextureResourceLoader>(AssetType::Image, "Image", _dir, _db);
+    registerLoader<MaterialData, MaterialResourceLoader>(AssetType::Material, "Material", _dir, _db, *this);
 }
-}
+
+} // namespace dk
--- src/runtime/resource/cpu/ResourceLoader.h
+++ src/runtime/resource/cpu/ResourceLoader.h
@@ -1,32 +1,114 @@
-﻿#pragma once
+#pragma once
+#include <filesystem>
+#include <memory>
+#include <string>
+#include <typeindex>
+#include <unordered_map>
+#include <utility>
+
+#include <fmt/base.h>
+
 #include "AssetDB.h"
-#include "ResourceCache.h"
-#include "MeshLoader.h"          // 读取 rawmesh
-#include "TextureLoader.h"
 #include "MaterialLoader.h"
+#include "MeshLoader.h"          // 读取 rawmesh
 #include "RawTypes.hpp"
+#include "ResourceCache.h"
+#include "TextureLoader.h"
 
 namespace dk {
 
+enum class AssetType
+{
+    Mesh,
+    Image,
+    Material,
+};
 
+class IResourceLoaderBase
+{
+public:
+    virtual ~IResourceLoaderBase() = default;
+};
 
+template <typename Res>
+class IResourceLoader : public IResourceLoaderBase
+{
+public:
+    virtual ~IResourceLoader() = default;
+    virtual std::shared_ptr<Res> load(UUID id) = 0;
+};
 
 class ResourceLoader
 {
 public:
-    ResourceLoader(const std::string& rawDir,
-                   AssetDB&           db, ResourceCache& cache)
-        : _dir(rawDir), _db(db), _cache(cache)
-    {
-    }
+    ResourceLoader(const std::string& rawDir, AssetDB& db, ResourceCache& cache);
 
-    std::shared_ptr<MeshData> loadMesh(UUID);
-    std::shared_ptr<TextureData>  loadImage(UUID);
-    std::shared_ptr<MaterialData> loadMaterial(UUID);
+    template <typename Res>
+    std::shared_ptr<Res> load(UUID id);
 
 private:
+    struct RegisteredType
+    {
+        AssetType    type;
+        std::string  name;
+        std::type_index type_id;
+    };
+
+    template <typename Res, typename Loader, typename... Args>
+    void registerLoader(AssetType type, std::string name, Args&&... args);
+
+    template <typename Res>
+    RegisteredType* registeredType();
+
     std::filesystem::path _dir;
     AssetDB&              _db;
     ResourceCache&        _cache;
+    std::unordered_map<AssetType, std::unique_ptr<IResourceLoaderBase>> _loaders;
+    std::unordered_map<std::type_index, RegisteredType>                 _typeRegistry;
 };
+
+template <typename Res>
+std::shared_ptr<Res> ResourceLoader::load(UUID id)
+{
+    auto* info = registeredType<Res>();
+    if (!info) return {};
+
+    auto it = _loaders.find(info->type);
+    if (it == _loaders.end())
+    {
+        fmt::print(stderr, "ResourceLoader: loader for type {} ({}) missing\n", info->name, typeid(Res).name());
+        return {};
+    }
+
+    auto* loader = dynamic_cast<IResourceLoader<Res>*>(it->second.get());
+    if (!loader)
+    {
+        fmt::print(stderr, "ResourceLoader: loader for type {} ({}) has incompatible interface\n", info->name, typeid(Res).name());
+        return {};
+    }
+
+    return _cache.resolve<Res>(id, [&] { return loader->load(id); });
 }
+
+template <typename Res, typename Loader, typename... Args>
+void ResourceLoader::registerLoader(AssetType type, std::string name, Args&&... args)
+{
+    auto type_id = std::type_index(typeid(Res));
+    _typeRegistry.emplace(type_id, RegisteredType{type, std::move(name), type_id});
+    _loaders.emplace(type, std::make_unique<Loader>(std::forward<Args>(args)...));
+}
+
+template <typename Res>
+ResourceLoader::RegisteredType* ResourceLoader::registeredType()
+{
+    auto type_id = std::type_index(typeid(Res));
+    auto it      = _typeRegistry.find(type_id);
+    if (it == _typeRegistry.end())
+    {
+        fmt::print(stderr, "ResourceLoader: loader for type {} not registered\n", typeid(Res).name());
+        return nullptr;
+    }
+    return &it->second;
+}
+
+} // namespace dk
